0b1f56f4d7a89a714c380c88f337d7f22c34457f
Craig <craig@oppkey.com>
2025-12-11 15:20:46 -1000

    rebuilt for python 3.12

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -52,7 +52,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "1489117476",
+        serviceWorkerVersion: "1067574692",
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
